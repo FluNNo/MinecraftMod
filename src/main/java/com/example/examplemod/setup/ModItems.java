f26bc978c7d9e2491431b3ad062b2326ad44bf90
NutroxModding <Henrik.Koepcke@web.de>
2021-02-21 19:17:43 +0100

    Kuchen für die Welt!

--- src/main/java/com/example/examplemod/setup/ModItems.java
+++ src/main/java/com/example/examplemod/setup/ModItems.java
@@ -1,19 +1,16 @@
 package com.example.examplemod.setup;
 
+import net.minecraft.item.Food;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraftforge.fml.RegistryObject;
 
 public class ModItems {
 
-    /*public static final RegistryObject<Item> CHAMELEON_BLOCK = RegistryHandler.ITEMS.register("chameleon_block", () ->
-        new Item(new Item.Properties().group(ItemGroup.MATERIALS)));*/
-
     public static final  RegistryObject<Item> CHAMELEON_INGOT = RegistryHandler.ITEMS.register("chameleon_ingot", () ->
             new Item((new Item.Properties().group(ItemGroup.MATERIALS))));
-
-    /*public static final  RegistryObject<Item> CHAMELEON_ORE = RegistryHandler.ITEMS.register("chameleon_ore", () ->
-            new Item((new Item.Properties().group(ItemGroup.MATERIALS))));*/
 
     public static final  RegistryObject<Item> CONTAINED_MATTER = RegistryHandler.ITEMS.register("contained_matter", () ->
             new Item((new Item.Properties().group(ItemGroup.MATERIALS))));
@@ -21,11 +18,14 @@ public class ModItems {
     public static final  RegistryObject<Item> CONTAINED_MATTER_UNSTABLE = RegistryHandler.ITEMS.register("contained_matter_unstable", () ->
             new Item((new Item.Properties().group(ItemGroup.MATERIALS))));
 
-    /*public static final  RegistryObject<Item> CRAFTING_STATION_V1 = RegistryHandler.ITEMS.register("crafting_station_v1", () ->
-            new Item((new Item.Properties().group(ItemGroup.MATERIALS))));*/
-
     public static final  RegistryObject<Item> MATTER = RegistryHandler.ITEMS.register("matter", () ->
             new Item((new Item.Properties().group(ItemGroup.MATERIALS))));
+
+    public static final  RegistryObject<Item> MUTZEN = RegistryHandler.ITEMS.register("mutzen", () ->
+            new Item((new Item.Properties().group(ItemGroup.FOOD).food(new Food.Builder().effect(() -> new EffectInstance(Effects.SPEED, 200, 1),1.0f).fastToEat().hunger(2).saturation(0.6f).build()))));
+
+    public static final  RegistryObject<Item> MUTZEN_XL = RegistryHandler.ITEMS.register("mutzen_xl", () ->
+            new Item((new Item.Properties().group(ItemGroup.FOOD).food(new Food.Builder().effect(() -> new EffectInstance(Effects.SPEED, 200, 2),1.0f).fastToEat().hunger(18).saturation(3.0f).build()))));
 
     static void register() {}
 }
