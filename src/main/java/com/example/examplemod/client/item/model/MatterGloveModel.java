e327fd8410fee37e03abe1c348af76361a45c470
Nocti <it9-koepha@student.itech-bs14.de>
2021-02-24 21:20:06 +0100

    pl0x dont copy and forget to change shit

--- src/main/java/com/example/examplemod/client/item/model/MatterGloveModel.java
+++ src/main/java/com/example/examplemod/client/item/model/MatterGloveModel.java
@@ -1,25 +1,25 @@
 package com.example.examplemod.client.item.model;
 
+import com.example.examplemod.ExampleMod;
 import com.example.examplemod.item.MatterGloveItem;
 import net.minecraft.util.ResourceLocation;
-import software.bernie.geckolib3.GeckoLib;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 
 public class MatterGloveModel extends AnimatedGeoModel<MatterGloveItem> {
 
     @Override
     public ResourceLocation getModelLocation(MatterGloveItem matterGloveItem) {
-        return new ResourceLocation(GeckoLib.ModID, "geo/matter_glove.geo.json");
+        return new ResourceLocation(ExampleMod.MODID, "geo/matter_glove.geo.json");
     }
 
     @Override
     public ResourceLocation getTextureLocation(MatterGloveItem matterGloveItem) {
-        return new ResourceLocation(GeckoLib.ModID, "textures/items/matter_glove.png");
+        return new ResourceLocation(ExampleMod.MODID, "textures/items/matter_glove.png");
     }
 
     @Override
     public ResourceLocation getAnimationFileLocation(MatterGloveItem matterGloveItem) {
-        return new ResourceLocation(GeckoLib.ModID, "animations/matter_glove.animation.json");
+        return new ResourceLocation(ExampleMod.MODID, "animations/matter_glove.animation.json");
     }
 
 }
